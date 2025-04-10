a2f2a582ecd87ba6f2ec77e500a8cc40ee82411c
Adam Schumperli <aschu042@ucr.edu>
2025-04-10 15:26:14 -0700

    added the fourth box, rounded the corners of the outer box and fully rounded the two middle boxes, changed the gap between boxes, changed font size and color, properly set up columns and changed the color of the text boxes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Example from "@/components/Example";
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <Example text1="Adjust" text2="these" text3="Props" />
+      <Example text1="Some" text2="Props" text3="Here" />
     </div>
   );
 };
--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,12 +1,18 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="flex w-3/4 items-center gap-1 bg-red-200 p-10">
-      <div className="w-1/2 bg-green-200 p-5 text-left">{props.text1}</div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="w-full bg-green-200 p-5 text-left">props.text2</div>
-        <div className="w-full bg-green-200 p-5 text-right">{props.text2}</div>
+    <div className="font-rsd-main flex w-2/3 flex-col items-center gap-8 rounded-4xl bg-red-200 p-10 text-2xl">
+      <div className="w-1/2 bg-sky-200 p-5 text-center">{props.text1}</div>
+      <div className="flex w-1/2 flex-row gap-2">
+        <div className="w-full rounded-full bg-cyan-200 p-5 text-center">
+          {props.text2}
+        </div>
+        <div className="w-full rounded-full bg-cyan-200 p-5 text-center">
+          {props.text2}
+        </div>
       </div>
-      {/* Hint: Add another box/div here similarly to line 4 */}
+      <div className="w-1/2 bg-blue-200 p-5 text-center text-white">
+        {props.text3}
+      </div>
     </div>
   );
 };
