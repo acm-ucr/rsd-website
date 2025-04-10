fb5a75210a73e50481c3f03869c2f8ffef0a87d6
stanleyx027 <sxiao027@ucr.edu>
2025-04-10 02:52:32 -0700

    Added a 4th box, changed layout to column, updated colors, size, and font, changed gaps

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,12 +1,20 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="flex w-3/4 items-center gap-1 bg-red-200 p-10">
-      <div className="w-1/2 bg-green-200 p-5 text-left">{props.text1}</div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="w-full bg-green-200 p-5 text-left">props.text2</div>
-        <div className="w-full bg-green-200 p-5 text-right">{props.text2}</div>
+    <div className="flex w-2/3 flex-col items-center gap-10 rounded-lg bg-red-200 p-10">
+      <div className="bg-rsd-light-blue font-rsd-main w-1/2 p-5 text-center text-3xl font-normal">
+        {props.text1}
       </div>
-      {/* Hint: Add another box/div here similarly to line 4 */}
+      <div className="flex w-1/2 flex-row gap-x-5 gap-y-0">
+        <div className="bg-rsd-mid-blue font-rsd-alt w-1/2 rounded-full p-5 text-center text-3xl font-normal">
+          {props.text2}
+        </div>
+        <div className="bg-rsd-mid-blue font-rsd-alt w-1/2 rounded-full p-5 text-center text-3xl font-normal">
+          {props.text2}
+        </div>
+      </div>
+      <div className="bg-rsd-title-blue font-rsd-main w-1/2 p-5 text-center text-3xl font-normal text-white">
+        {props.text3}
+      </div>
     </div>
   );
 };
