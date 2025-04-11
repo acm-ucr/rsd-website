147a2aa401d9f800234847a6f4982ddc0d90cfaa
LawMot <molawr12@gmail.com>
2025-04-10 21:06:34 -0700

    created navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,30 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/public/RSDLogo.webp";
+import { items } from "@/data/NavbarData";
+import { usePathname } from "next/navigation";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const pathname = usePathname();
+  return (
+    <div className="flex w-full items-center justify-between bg-[#6694C9] px-5 py-4">
+      <Image src={logo} alt="RSD Logo" className="w-1/20" />
+      <div className="mx-5 grid grid-flow-col gap-6 text-2xl text-white">
+        {items.map(({ name, link }) => (
+          <Link
+            key={name}
+            href={link}
+            className={`${
+              pathname === link ? "text-[#F7C55B]" : "hover:text-gray-300"
+            }`}
+          >
+            {name}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
