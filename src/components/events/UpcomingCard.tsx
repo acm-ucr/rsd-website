935b7a2f8b271a61d7dca4e8d80b2ac15b010be8
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-04-15 22:52:06 -0700

    Added ComponentProps interface, formatted component file

--- src/components/events/UpcomingCard.tsx
+++ src/components/events/UpcomingCard.tsx
@@ -1,11 +1,18 @@
-const UpcomingCard = ({month, date, name, desc}) => {
+interface ComponentProps {
+  month: string;
+  date: string;
+  name: string;
+  desc: string;
+}
+
+const UpcomingCard = ({ month, date, name, desc }: ComponentProps) => {
   return (
-    <div className="flex w-[770px] h-[371px] rounded-[70] bg-rsd-dark-blue text-white p-10 shadow-md">
-      <div className="flex flex-col w-[264px] h-[297px] rounded-[50px] bg-white text-black text-center font-rsd-alt p-10 justify-center items-center shrink-0">
+    <div className="bg-rsd-dark-blue flex h-[371px] w-[770px] rounded-[70] p-10 text-white shadow-md">
+      <div className="font-rsd-alt flex h-[297px] w-[264px] shrink-0 flex-col items-center justify-center rounded-[50px] bg-white p-10 text-center text-black">
         <div className="text-[40px] font-normal">{month}</div>
         <div className="text-[96px] font-normal">{date}</div>
       </div>
-      <div className="flex flex-col text-white text-left font-rsd-alt justify-start p-10 gap-y-5">
+      <div className="font-rsd-alt flex flex-col justify-start gap-y-5 p-10 text-left text-white">
         <div className="text-[40px] font-bold">{name}</div>
         <div className="text-[20px] font-bold">{desc}</div>
       </div>
