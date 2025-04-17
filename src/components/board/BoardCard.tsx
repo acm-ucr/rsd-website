bed450f4dbf5459418a91532d70d2dc95216b860
stanleyx027 <sxiao027@ucr.edu>
2025-04-17 12:49:52 -0700

    Completed the changes except for the react-icons

--- src/components/board/BoardCard.tsx
+++ src/components/board/BoardCard.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Temp from "@/public/RSDLogo.webp";
-import { IoIosMail } from "react-icons/io";
+import { Mail } from "lucide-react";
 import { FaDiscord } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 
@@ -11,20 +11,24 @@ interface BoardCardProps {
 
 const BoardCard = ({ name, role }: BoardCardProps) => {
   return (
-    <div className="to-rsd-dark-blue flex h-[615px] w-[504.4px] flex-col place-items-center rounded-[20px] bg-radial from-[#074CB5] via-[#002f74]">
-      <div className="mt-19 flex h-[304.82px] w-[375.49px] justify-center">
-        <Image src={Temp} alt="Temp" className="rounded-[20px]" />
+    <div className="to-rsd-dark-blue from-rsd-title-blue flex h-10/12 w-4/12 flex-col place-items-center rounded-2xl bg-radial">
+      <div className="mt-18 flex h-7/12 w-9/12 flex-col items-center pt-4">
+        <Image
+          src={Temp}
+          alt="Temp"
+          className="h-11/12 w-12/12 flex-shrink-0 justify-center rounded-2xl"
+        />
       </div>
-      <p className="font-rsd-alt flex h-[75px] pt-8 text-center text-[40px] leading-none font-bold text-[#FFCC61]">
+      <p className="font-rsd-alt text-rsd-yellow flex justify-center pt-1 text-center text-4xl leading-none font-bold">
         {name}
       </p>
-      <p className="font-rsd-alt flex h-[75px] pt-2 text-center text-[32px] leading-none font-bold text-[#E8ECF1]">
+      <p className="font-rsd-alt flex pt-2 text-center text-3xl leading-none font-bold text-white">
         {role}
       </p>
-      <div className="mb-12 ml-72 flex h-[54.53px] w-[143.62px] justify-end">
-        <IoIosMail className="-mr-1.5 flex h-[63.7px] w-[57.87px] fill-[#E8ECF1] pt-0" />
-        <AiFillInstagram className="mr-0.5 flex h-[63.7px] w-[57.87px] fill-[#E8ECF1] pt-0" />
-        <FaDiscord className="-ml-1.5 flex h-[63.7px] w-[57.87px] pt-0 text-[#E8ECF1]" />
+      <div className="flex w-5/6 place-items-end justify-end gap-0 pt-4">
+        <Mail className="flex h-11/12 w-1/8 text-white" />
+        <AiFillInstagram className="flex text-5xl text-white" />
+        <FaDiscord className="flex text-5xl text-white" />
       </div>
     </div>
   );
