c1b6508aaf2455a6c4a4fa00be92050c66b55aab
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-04-17 00:48:11 -0700

    Formatting and minor adjustments

--- src/components/events/UpcomingCard.tsx
+++ src/components/events/UpcomingCard.tsx
@@ -7,12 +7,12 @@ interface ComponentProps {
 
 const UpcomingCard = ({ month, date, name, desc }: ComponentProps) => {
   return (
-    <div className="bg-rsd-dark-blue flex h-1/2 w-3/5 rounded-4xl p-10 text-white shadow-md">
-      <div className="font-rsd-alt flex h-full w-1/3 shrink-0 flex-col items-center justify-center rounded-3xl bg-white p-10 text-center text-black gap-y-8">
+    <div className="bg-rsd-dark-blue font-rsd-alt flex h-1/2 w-3/5 rounded-4xl p-10 text-white shadow-md">
+      <div className="flex h-full w-1/3 shrink-0 flex-col items-center justify-center gap-y-8 rounded-3xl bg-white p-10 text-center text-black">
         <div className="text-5xl font-normal">{month}</div>
         <div className="text-8xl font-normal">{date}</div>
       </div>
-      <div className="font-rsd-alt flex flex-col items-start justify-center gap-y-10 p-10 text-left text-white">
+      <div className="mt-8 flex h-full w-2/3 shrink-0 flex-col items-start gap-y-8 p-10 text-left text-white">
         <div className="text-5xl font-bold">{name}</div>
         <div className="text-xl font-bold">{desc}</div>
       </div>
