73c4e0928ac125d0674ca0e3745fd26cf66a5d32
Adam Schumperli <aschu042@ucr.edu>
2025-04-17 16:38:21 -0700

    created race results carousel
    fix merge conflict

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,5 +1,15 @@
-const InfoBox = () => {
-  return <div>InfoBox</div>;
+//import Image, { StaticImageData } from "next/image";
+interface InfoBoxProps {
+  text: React.ReactNode;
+}
+const InfoBox: React.FC<InfoBoxProps> = ({ text }) => {
+  return (
+    <div className="relative my-8 w-full px-7 text-left">
+      <div className="font-rsd-alt rounded-4xl bg-blue-950 px-20 py-20 text-2xl font-bold text-white">
+        {text}
+      </div>
+    </div>
+  );
 };
 
 export default InfoBox;
