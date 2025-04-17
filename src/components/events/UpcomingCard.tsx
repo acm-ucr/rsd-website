fc7ddc2d447bf4cc04d968c030efedd64cf79483
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-04-17 11:30:28 -0700

    Removed font-normal from month and date divs, normal is default so not necessary

--- src/components/events/UpcomingCard.tsx
+++ src/components/events/UpcomingCard.tsx
@@ -9,8 +9,8 @@ const UpcomingCard = ({ month, date, name, desc }: ComponentProps) => {
   return (
     <div className="bg-rsd-dark-blue font-rsd-alt flex h-1/2 w-3/5 rounded-4xl p-10 text-white shadow-md">
       <div className="flex h-full w-1/3 shrink-0 flex-col items-center justify-center gap-y-8 rounded-3xl bg-white p-10 text-center text-black">
-        <div className="text-5xl font-normal">{month}</div>
-        <div className="text-8xl font-normal">{date}</div>
+        <div className="text-5xl">{month}</div>
+        <div className="text-8xl">{date}</div>
       </div>
       <div className="mt-8 flex h-full w-2/3 shrink-0 flex-col items-start gap-y-8 p-10 text-left text-white">
         <div className="text-5xl font-bold">{name}</div>
