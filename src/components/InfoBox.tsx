dd45013b573caa9220b4976aabdc824f9248e802
Adam Schumperli <aschu042@ucr.edu>
2025-04-17 19:03:20 -0700

    made a prop of type React.ReactNode which can be used as a general text box throughout the website, I specifically tested with a text box in the about page.

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -4,8 +4,8 @@ interface InfoBoxProps {
 }
 const InfoBox: React.FC<InfoBoxProps> = ({ text }) => {
   return (
-    <div className="relative my-8 w-full px-7 text-left">
-      <div className="font-rsd-alt rounded-4xl bg-blue-950 px-20 py-20 text-2xl font-bold text-white">
+    <div className="my-8 w-full px-7 text-left">
+      <div className="font-rsd-alt rounded-4xl bg-rsd-dark-blue px-20 py-20 text-2xl font-bold text-white">
         {text}
       </div>
     </div>
