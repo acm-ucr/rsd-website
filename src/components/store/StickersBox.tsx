4bc70d3bbd558c2a62d682d6a5601d47aafbd751
ALE00111 <austintle56@gmail.com>
2025-04-18 00:45:06 -0700

    Adding stickersbox

--- src/components/store/StickersBox.tsx
+++ src/components/store/StickersBox.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+import DragonFrame from "@/public/store/dragonFrame.webp";
+
 const StickersBox = () => {
-  return <div>StickersBox</div>;
+  return (
+    <div className="flex justify-center">
+      <div className="relative flex w-2/3 items-center justify-center">
+        <Image src={DragonFrame} alt="Outer Dragon Frame" />
+        <div className="bg-rsd-mid-blue absolute flex h-3/4 w-3/4 items-center justify-center rounded-3xl">
+          <p className="font-rsd-main text-center text-base font-bold md:text-4xl">
+            Come visit us at tabling for stickers!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StickersBox;
