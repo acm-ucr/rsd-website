96fb650addf2c3b8a1d4826df721d67bc96a8f73
LawMot <molawr12@gmail.com>
2025-04-18 08:07:34 -0700

    created race results carousel
    fix merge conflict

--- src/data/ResultsInfo.ts
+++ src/data/ResultsInfo.ts
@@ -1,5 +1,3 @@
-//PLACEHOLDER FOR IMAGES JUST PURELY FOR TESTING
-
 export const slides: slide[] = [
   { id: 0, year: 2022, title: "Event Name", subtitle: "Event Placement Link" },
   { id: 1, year: 2023, title: "Event Name", subtitle: "Event Placement Link" },
