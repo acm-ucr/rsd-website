1274d64d7e4fa4d6105866571d0e8df4262c5cc5
Nathan Wang <nathanbchenwang@gmail.com>
2025-04-18 00:42:59 -0700

    finished creating the award component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Example from "@/components/Example";
-
+//import Example from "@/components/Example";
+import Carousel from "@/components/home/GalleryPreview";
+import AwardCard from "@/components/home/AwardCard";
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <Example text1="Some" text2="Props" text3="Here" />
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      {/* <Example text1="Adjust" text2="these" text3="Props" /> */}
+      <Carousel />
+
+      <AwardCard AwardName={"Award\nName"} />
     </div>
   );
 };
---
+++ src/components/home/AwardCard.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Award from "@/public/doublePaddle.webp";
+interface AwardCardProps {
+  AwardName: string;
+}
+const AwardCard = ({ AwardName }: AwardCardProps) => {
+  return (
+    <div className="font-rsd-main flex h-60 w-50 flex-col items-center gap-10 bg-blue-200 p-10 text-xl">
+      <div className="h-30 w-30 -translate-y-4 rounded-full bg-neutral-400">
+        <Image src={Award} alt="Award" className="mask-radial-at-center" />
+      </div>
+      <p className="font-rsd-alt -mt-10 text-center text-2xl font-bold text-blue-700">
+        {AwardName}
+      </p>
+    </div>
+  );
+};
+
+export default AwardCard;
