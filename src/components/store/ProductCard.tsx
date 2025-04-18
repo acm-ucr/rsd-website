1500f44f2bca42ba67177ad66c8eada149e21b66
Nina <nina@ucr-secure-48-10-13-242-212.wnet.ucr.edu>
2025-04-18 13:06:17 -0700

    merch card component and calling it in store page

--- src/components/store/ProductCard.tsx
+++ src/components/store/ProductCard.tsx
@@ -1,5 +1,16 @@
-const ProductCard = () => {
-  return <div>ProductCard</div>;
+import Image from "next/image";
+import product from "@/public/singlePaddle.webp";
+
+const ProductCard = (props: { text1: string; text2: string }) => {
+  return (
+    <div className="font-rsd-alt flex-col items-center p-10 text-xl">
+      <div className="bg-rsd-light-blue rounded-2xl">
+        <Image src={product} alt="product" />
+      </div>
+      <div className="p-2 text-center text-3xl">{props.text1}</div>
+      <div className="text-center text-2xl">{props.text2}</div>
+    </div>
+  );
 };
 
 export default ProductCard;
