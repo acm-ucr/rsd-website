185da7d045d03c91d376873c60aafa95a5a13ac7
ALE00111 <austintle56@gmail.com>
2025-04-19 15:58:14 -0700

    Updating stickersbox

--- src/components/store/StickersBox.tsx
+++ src/components/store/StickersBox.tsx
@@ -4,10 +4,10 @@ import DragonFrame from "@/public/store/dragonFrame.webp";
 const StickersBox = () => {
   return (
     <div className="flex justify-center">
-      <div className="relative flex w-2/3 items-center justify-center">
+      <div className="relative flex w-fit items-center justify-center">
         <Image src={DragonFrame} alt="Outer Dragon Frame" />
-        <div className="bg-rsd-mid-blue absolute flex h-3/4 w-3/4 items-center justify-center rounded-3xl">
-          <p className="font-rsd-main text-center text-base font-bold md:text-4xl">
+        <div className="bg-rsd-mid-blue absolute flex h-3/4 w-fit items-center rounded-3xl bg-radial from-white px-3 md:mx-24 md:px-64">
+          <p className="font-rsd-main text-center font-bold lg:text-4xl">
             Come visit us at tabling for stickers!
           </p>
         </div>
