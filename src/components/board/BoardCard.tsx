4ec4be1cb6088afbb0bcc1f43dcfcef8810e5dbd
qhgill <qhgill2@gmail.com>
2025-04-19 00:28:14 -0700

    minor changes my fault

--- src/components/board/BoardCard.tsx
+++ src/components/board/BoardCard.tsx
@@ -11,17 +11,17 @@ interface BoardCardProps {
 
 const BoardCard = ({ name, role }: BoardCardProps) => {
   return (
-    <div className="to-rsd-dark-blue from-rsd-title-blue flex h-4/5 w-4/12 flex-col place-items-center rounded-3xl bg-radial p-15">
+    <div className="to-rsd-dark-blue from-rsd-title-blue flex w-[30vw] flex-col items-center rounded-3xl bg-radial p-15">
       <Image
         src={Temp}
         alt="Temp"
-        className="flex aspect-square h-80 w-96 flex-shrink-0 justify-center rounded-2xl object-fill"
+        className="flex aspect-square flex-shrink-0 justify-center rounded-2xl object-fill"
       />
       <div className="font-rsd-alt flex flex-col items-center p-5 text-center font-bold">
         <div className="text-rsd-yellow flex text-4xl">{name}</div>
         <div className="flex text-center text-3xl text-white">{role}</div>
       </div>
-      <div className="ml-72 flex flex-row place-items-end justify-end p-2">
+      <div className="flex w-full flex-row place-items-end justify-end p-2">
         <Mail className="flex h-12 w-12 text-white" />
         <AiFillInstagram className="flex text-5xl text-white" />
         <FaDiscord className="flex text-5xl text-white" />
