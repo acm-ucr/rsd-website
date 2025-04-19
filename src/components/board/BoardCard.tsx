3813befe4c5bbb564dad84bc607b92de2131ca8e
stanleyx027 <sxiao027@ucr.edu>
2025-04-18 22:52:14 -0700

    Fix the image and icon sizing

--- src/components/board/BoardCard.tsx
+++ src/components/board/BoardCard.tsx
@@ -11,22 +11,18 @@ interface BoardCardProps {
 
 const BoardCard = ({ name, role }: BoardCardProps) => {
   return (
-    <div className="to-rsd-dark-blue from-rsd-title-blue flex h-10/12 w-4/12 flex-col place-items-center rounded-2xl bg-radial">
-      <div className="mt-18 flex h-7/12 w-9/12 flex-col items-center pt-4">
-        <Image
-          src={Temp}
-          alt="Temp"
-          className="h-11/12 w-12/12 flex-shrink-0 justify-center rounded-2xl"
-        />
+    <div className="to-rsd-dark-blue from-rsd-title-blue flex h-4/5 w-4/12 flex-col place-items-center rounded-3xl bg-radial p-15">
+      <Image
+        src={Temp}
+        alt="Temp"
+        className="flex aspect-square h-80 w-96 flex-shrink-0 justify-center rounded-2xl object-fill"
+      />
+      <div className="font-rsd-alt flex flex-col items-center p-5 text-center font-bold">
+        <div className="text-rsd-yellow flex text-4xl">{name}</div>
+        <div className="flex text-center text-3xl text-white">{role}</div>
       </div>
-      <p className="font-rsd-alt text-rsd-yellow flex justify-center pt-1 text-center text-4xl leading-none font-bold">
-        {name}
-      </p>
-      <p className="font-rsd-alt flex pt-2 text-center text-3xl leading-none font-bold text-white">
-        {role}
-      </p>
-      <div className="flex w-5/6 place-items-end justify-end gap-0 pt-4">
-        <Mail className="flex h-11/12 w-1/8 text-white" />
+      <div className="ml-72 flex flex-row place-items-end justify-end p-2">
+        <Mail className="flex h-12 w-12 text-white" />
         <AiFillInstagram className="flex text-5xl text-white" />
         <FaDiscord className="flex text-5xl text-white" />
       </div>
