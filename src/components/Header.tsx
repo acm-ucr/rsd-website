384c938928fd47a2f0393731ec9756db88fa851f
Adam Schumperli <aschu042@ucr.edu>
2025-04-22 14:00:31 -0700

    made a proper header that is reusable

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,14 @@
-const Header = () => {
-  return <div>Header</div>;
+interface HeaderProps {
+  text: string;
+}
+const Header = ({ text }: HeaderProps) => {
+  return (
+    <div className="my-8 w-full px-7 text-center">
+      <div className="font-rsd-main text-rsd-title-blue text-[70px]">
+        {text}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
