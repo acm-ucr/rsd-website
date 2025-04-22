7a3f86a5879f552523190afc4766c1e0cdb30861
qhgill <qhgill2@gmail.com>
2025-04-21 22:51:25 -0700

    destructuring

--- src/components/store/ProductCard.tsx
+++ src/components/store/ProductCard.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 import product from "@/public/singlePaddle.webp";
 
 interface ProductTypes {
-  productName: string,
-  productPrice: string
+  productName: string;
+  productPrice: string;
 }
 
-const ProductCard = ({ productName, productPrice}: ProductTypes) => {
+const ProductCard = ({ productName, productPrice }: ProductTypes) => {
   return (
     <div className="font-rsd-alt flex-col items-center p-10 text-xl">
       <div className="bg-rsd-light-blue h-96 w-full min-w-0 rounded-2xl">
