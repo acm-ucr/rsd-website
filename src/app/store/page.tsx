ce66fb2d9101f0bb1e595c05631a499f26f1bac8
Nina <nina@ucr-secure-48-10-13-242-212.wnet.ucr.edu>
2025-04-21 20:57:06 -0700

    changed var names, set max height

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,5 +1,14 @@
+import ProductCard from "@/components/store/ProductCard";
+
+
 const Store = () => {
-  return <div>Store</div>;
+ return (
+   <div className="row flex h-screen w-screen items-center justify-center">
+     <ProductCard productName="Shirt" productPrice="17.99$" />
+     <ProductCard productName="Jacket" productPrice="29.99$" />
+   </div>
+ );
 };
+
 
 export default Store;
--- src/components/store/ProductCard.tsx
+++ src/components/store/ProductCard.tsx
@@ -1,5 +1,18 @@
-const ProductCard = () => {
-  return <div>ProductCard</div>;
+import Image from "next/image";
+import product from "@/public/singlePaddle.webp";
+
+
+const ProductCard = (props: { productName: string; productPrice: string }) => {
+ return (
+   <div className="font-rsd-alt flex-col items-center p-10 text-xl">
+     <div className="bg-rsd-light-blue rounded-2xl h-96 w-full min-w-0">
+      <Image className="max-h-96" src={product} alt="product" />
+     </div>
+     <div className="p-2 text-center text-3xl">{props.productName}</div>
+     <div className="text-center text-2xl">{props.productPrice}</div>
+   </div>
+ );
 };
+
 
 export default ProductCard;
