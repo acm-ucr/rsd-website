fce67c83745dd3eb7ec6ad2143805a90af73ed21
qhgill <qhgill2@gmail.com>
2025-04-23 20:49:40 -0700

    tailwind textsize

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,7 @@ interface HeaderProps {
 const Header = ({ text }: HeaderProps) => {
   return (
     <div className="m-8 w-full text-center">
-      <div className="font-rsd-main text-rsd-title-blue text-[70px]">
-        {text}
-      </div>
+      <div className="font-rsd-main text-rsd-title-blue text-7xl">{text}</div>
     </div>
   );
 };
