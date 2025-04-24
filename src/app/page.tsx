f2c87c96b8ef61a26e031487e81c488110598270
qhgill <qhgill2@gmail.com>
2025-04-23 22:36:49 -0700

    removed example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-//import Example from "@/components/Example";
 import Carousel from "@/components/home/GalleryPreview";
 
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      {/* <Example text1="Adjust" text2="these" text3="Props" /> */}
       <Carousel />
     </div>
   );
