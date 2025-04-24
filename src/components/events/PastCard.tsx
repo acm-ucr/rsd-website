059646fb180367f35a7890f425c16271d7f82067
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-04-24 16:47:53 -0700

    Proper formatting

--- src/components/events/PastCard.tsx
+++ src/components/events/PastCard.tsx
@@ -1,20 +1,20 @@
-import Image, { StaticImageData } from "next/image"
+import Image, { StaticImageData } from "next/image";
 interface ComponentProps {
   image: StaticImageData;
   eventName: string;
 }
 
-const PastCard = ( {eventName, image}: ComponentProps) => {
+const PastCard = ({ eventName, image }: ComponentProps) => {
   return (
-    <div className="flex flex-col w-1/4 h-1/2 font-rsd-alt">
-      <div className="h-4/5 bg-gray-300 rounded-t-2xl aspect-[4/3]">
-        <Image 
-        src={image}
-        alt="Image of past event"
-        className="w-full h-full object-contain"
+    <div className="font-rsd-alt flex h-1/2 w-1/4 flex-col">
+      <div className="aspect-[4/3] h-4/5 rounded-t-2xl bg-gray-300">
+        <Image
+          src={image}
+          alt="Image of past event"
+          className="h-full w-full object-contain"
         />
       </div>
-      <div className="flex items-center justify-center bg-white h-1/5 rounded-b-2xl">
+      <div className="flex h-1/5 items-center justify-center rounded-b-2xl bg-white">
         <div className="text-4xl">{eventName}</div>
       </div>
     </div>
