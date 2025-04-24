83d151300e588c739c00124b8dff8c2ac5edb727
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-04-24 16:00:22 -0700

    Started PastCard component, initalized basic info

--- src/components/events/PastCard.tsx
+++ src/components/events/PastCard.tsx
@@ -1,5 +1,13 @@
-const PastCard = () => {
-  return <div>PastCard</div>;
+import Image, { StaticImageData } from "public/RSDLogo"
+interface ComponentProps {
+  image: StaticImageData;
+  eventName: string;
+}
+
+const PastCard = ( {eventName, image}: ComponentProps) => {
+  return (
+  <div> className="bg-rsd-light-blue"</div>
+  );
 };
 
 export default PastCard;
