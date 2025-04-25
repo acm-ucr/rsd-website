c5b35c0bc7a89a30fac984485135939ddc6639a9
Charleen Chen <charleenc485@gmail.com>
2025-04-24 22:34:20 -0700

    added assets

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,5 +1,12 @@
+import PastCard from "@/components/events/PastCard";
+import cloud from "@/public/cloud.webp";
+
 const Events = () => {
-  return <div>Events</div>;
+  return (
+    <div className="bg-amber-500 p-10">
+      <PastCard eventName="hi" image={cloud} />
+    </div>
+  );
 };
 
 export default Events;
