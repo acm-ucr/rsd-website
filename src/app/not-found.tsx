8795a90e0859fa8d5f6907379ed416b046a3cbaa
LawMot <molawr12@gmail.com>
2025-04-25 00:02:48 -0700

    add padding and spaced clouds

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,18 +7,18 @@ import { Button } from "@/components/ui/button";
 const NotFound = () => {
   return (
     <div className="bg-rsd-dark-blue flex h-screen w-screen flex-col items-center justify-center">
-      <Image src={cloud} alt="cloud" className="mr-120 w-1/4" />
+      <Image src={cloud} alt="cloud" className="mr-165 w-1/4" />
       <div className="text-rsd-gold mb-3 text-7xl font-semibold">404</div>
       <div className="font-rsd-alt mb-5 text-2xl text-white">
         Sorry, the page you were looking for is not found!
       </div>
       <Button
         asChild
-        className="bg-rsd-mid-blue/40 flex h-1/15 w-1/7 rounded-full text-2xl shadow-xl hover:bg-gray-600"
+        className="bg-rsd-mid-blue/40 flex h-1/15 w-1/7 rounded-full px-25 py-5 text-2xl shadow-xl hover:bg-gray-600"
       >
         <Link href="/">Go Back Home</Link>
       </Button>
-      <Image src={cloud} alt="cloud" className="ml-120 w-1/4" />
+      <Image src={cloud} alt="cloud" className="ml-165 w-1/4" />
     </div>
   );
 };
