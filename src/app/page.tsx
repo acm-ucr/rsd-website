70d057f493c68cc89b6e7e827c1e25c0c23322f5
Nathan Wang <nathanbchenwang@gmail.com>
2025-04-24 23:25:55 -0700

    reformatting page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Carousel from "@/components/home/GalleryPreview";
- 
+
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Carousel />
     </div>
-    
   );
 };
 
