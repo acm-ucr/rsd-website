1172424034a719c1c117278350f4fda873228666
LawMot <molawr12@gmail.com>
2025-04-24 20:37:02 -0700

    created 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,26 @@
+"use client";
+import cloud from "@/public/cloud.webp";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 const NotFound = () => {
-  return <div>404 Page Not Found</div>;
+  return (
+    <div className="bg-rsd-dark-blue flex h-screen w-screen flex-col items-center justify-center">
+      <Image src={cloud} alt="cloud" className="mr-120 w-1/4" />
+      <div className="text-rsd-gold mb-3 text-7xl font-semibold">404</div>
+      <div className="font-rsd-alt mb-5 text-2xl text-white">
+        Sorry, the page you were looking for is not found!
+      </div>
+      <Button
+        asChild
+        className="bg-rsd-mid-blue/40 flex h-1/15 w-1/7 rounded-full text-2xl shadow-xl hover:bg-gray-600"
+      >
+        <Link href="/">Go Back Home</Link>
+      </Button>
+      <Image src={cloud} alt="cloud" className="ml-120 w-1/4" />
+    </div>
+  );
 };
 
 export default NotFound;
