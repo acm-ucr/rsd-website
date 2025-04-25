df5aa33dcb3bcf2ed1467d8ed04a5fe66289ae61
Nina <nina@ucr-secure-48-10-13-241-93.wnet.ucr.edu>
2025-04-25 00:55:06 -0700

    created gallery card with random link

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,5 +1,12 @@
+import GalleryCard from "@/components/gallery/GalleryCard";
+
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <div className="row flex h-screen w-screen items-center gap-5 justify-center">
+        <GalleryCard photoName="Fall '24" link="https://en.wikipedia.org/wiki/Paddling" image="singlePaddle.webp"/>
+        <GalleryCard photoName="Winter '25" link="https://en.wikipedia.org/wiki/Paddling" image="singlePaddle.webp"/>
+    </div>
+  );
 };
 
 export default Gallery;
--- src/components/gallery/GalleryCard.tsx
+++ src/components/gallery/GalleryCard.tsx
@@ -1,5 +1,23 @@
-const GalleryCard = () => {
-  return <div>GalleryCard</div>;
+import Link from "next/link";
+import Image from "next/image";
+
+
+
+interface GalleryInfo {
+  photoName: string;
+  link: string;
+  image: string;
+}
+
+const GalleryCard = ({photoName, link, image}: GalleryInfo) => {
+  return (
+    <Link href={link} target="_blank">
+      <div className="font-rsd-alt flex-col items-center h-48 w-96 bg-gray-200 min-w-0 rounded-t-2xl">
+        <Image className="h-full w-full" src={image} alt="product" width={400} height={400}/>
+        <div className="rounded-b-2xl bg-white p-2 text-center text-xl">{photoName}</div>
+      </div>
+    </Link>
+  );
 };
 
 export default GalleryCard;
