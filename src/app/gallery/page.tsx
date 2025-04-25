40bcae173e42058443b3577fce82ff20df211322
Nina <nina@ucr-secure-48-10-13-241-93.wnet.ucr.edu>
2025-04-25 01:10:18 -0700

    created gallery card with random link

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -2,9 +2,17 @@ import GalleryCard from "@/components/gallery/GalleryCard";
 
 const Gallery = () => {
   return (
-    <div className="row flex h-screen w-screen items-center gap-5 justify-center">
-        <GalleryCard photoName="Fall '24" link="https://en.wikipedia.org/wiki/Paddling" image="singlePaddle.webp"/>
-        <GalleryCard photoName="Winter '25" link="https://en.wikipedia.org/wiki/Paddling" image="singlePaddle.webp"/>
+    <div className="row flex h-screen w-screen items-center justify-center gap-5">
+      <GalleryCard
+        photoName="Fall '24"
+        link="https://en.wikipedia.org/wiki/Paddling"
+        image="singlePaddle.webp"
+      />
+      <GalleryCard
+        photoName="Winter '25"
+        link="https://en.wikipedia.org/wiki/Paddling"
+        image="singlePaddle.webp"
+      />
     </div>
   );
 };
--- src/components/gallery/GalleryCard.tsx
+++ src/components/gallery/GalleryCard.tsx
@@ -1,20 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
 
-
-
 interface GalleryInfo {
   photoName: string;
   link: string;
   image: string;
 }
 
-const GalleryCard = ({photoName, link, image}: GalleryInfo) => {
+const GalleryCard = ({ photoName, link, image }: GalleryInfo) => {
   return (
     <Link href={link} target="_blank">
-      <div className="font-rsd-alt flex-col items-center h-48 w-96 bg-gray-200 min-w-0 rounded-t-2xl">
-        <Image className="h-full w-full" src={image} alt="product" width={400} height={400}/>
-        <div className="rounded-b-2xl bg-white p-2 text-center text-xl">{photoName}</div>
+      <div className="font-rsd-alt h-48 w-96 min-w-0 flex-col items-center rounded-t-2xl bg-gray-200">
+        <Image
+          className="h-full w-full"
+          src={image}
+          alt="product"
+          width={400}
+          height={400}
+        />
+        <div className="rounded-b-2xl bg-white p-2 text-center text-xl">
+          {photoName}
+        </div>
       </div>
     </Link>
   );
