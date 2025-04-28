3d3afef035fe2d5a2c3289fc0a07f08374aa01c1
Shrish Vishnu Rajesh Kumar <shrishvishnu06@gmail.com>
2025-04-28 06:21:51 -0700

    changed the color and how the icons are being mapped

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,42 +1,36 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { footerLinks } from "../data/FooterLinks";
-
-import { FaInstagram, FaDiscord, FaEnvelope } from "react-icons/fa";
+import { footerLinks } from "@/data/FooterLinks";
 
 const footerImage = "/RSDLogo.webp";
 
-const iconMap: Record<string, JSX.Element> = {
-  mail: <FaEnvelope className="h-6 w-6 transition-transform hover:scale-110" />,
-  instagram: (
-    <FaInstagram className="h-6 w-6 transition-transform hover:scale-110" />
-  ),
-  discord: (
-    <FaDiscord className="h-6 w-6 transition-transform hover:scale-110" />
-  ),
-};
+footerLinks.map((link) => (
+  <a href={link.url}>
+    <link.icon className="h-6 w-6 transition-transform hover:scale-110" />
+    {link.name}
+  </a>
+));
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-between bg-sky-400 px-6 py-4">
+    <div className="bg-rsd-mid-blue flex items-center justify-between px-6 py-4">
       <Link href="/" className="transition-opacity hover:opacity-80">
         <Image src={footerImage} alt="RSD Logo" width={100} height={50} />
       </Link>
 
       <div className="flex space-x-4">
-        {footerLinks.map(({ icon, url }, idx) =>
-          iconMap[icon] ? (
-            <Link
-              key={idx}
-              href={url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {iconMap[icon]}
-            </Link>
-          ) : null,
-        )}
+        {footerLinks.map(({ icon: Icon, url }, idx) => (
+          <Link
+            key={idx}
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-transform hover:scale-110"
+          >
+            <Icon className="h-8 w-8 text-white" />
+          </Link>
+        ))}
       </div>
     </div>
   );
--- src/data/FooterLinks.ts
+++ src/data/FooterLinks.ts
@@ -1,5 +1,12 @@
+import { FaInstagram, FaDiscord, FaEnvelope, FaYoutube } from "react-icons/fa";
+
 export const footerLinks = [
-  { icon: "mail", url: "mailto:example@example.com" },
-  { icon: "instagram", url: "https://instagram.com/yourprofile" },
-  { icon: "discord", url: "https://discord.gg/yourserver" },
+  { name: "Mail", icon: FaEnvelope, url: "mailto:sampleEmail@example.com" },
+  { name: "Discord", icon: FaDiscord, url: "https://discord.gg/aserver" },
+  {
+    name: "Instagram",
+    icon: FaInstagram,
+    url: "https://instagram.com/profile",
+  },
+  { name: "Youtube", icon: FaYoutube, url: "https://youtube.com" },
 ];
