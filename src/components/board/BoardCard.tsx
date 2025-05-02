8cf4f3422992d7feadf228fd24cc2e96efcf3b8a
Adam Schumperli <aschu042@ucr.edu>
2025-05-01 19:43:51 -0700

    Added the data for all the Board Cards, edited Board Cards to take in this data, and mapped the Board Cards to appear across the screen

--- src/components/board/BoardCard.tsx
+++ src/components/board/BoardCard.tsx
@@ -1,20 +1,32 @@
-import Image from "next/image";
-import Temp from "@/public/RSDLogo.webp";
+import Image, { StaticImageData } from "next/image";
+import RSDLogo from "@/public/RSDLogo.webp";
 import { Mail } from "lucide-react";
 import { FaDiscord } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
+import Link from "next/link";
 
 interface BoardCardProps {
   name: string;
   role: string;
+  image: StaticImageData;
+  email: string;
+  discord: string;
+  instagram: string;
 }
 
-const BoardCard = ({ name, role }: BoardCardProps) => {
+const BoardCard = ({
+  name,
+  role,
+  image,
+  email,
+  discord,
+  instagram,
+}: BoardCardProps) => {
   return (
     <div className="to-rsd-dark-blue from-rsd-title-blue flex w-[30vw] flex-col items-center rounded-3xl bg-radial p-15">
       <Image
-        src={Temp}
-        alt="Temp"
+        src={image || RSDLogo}
+        alt={name}
         className="flex aspect-square flex-shrink-0 justify-center rounded-2xl object-fill"
       />
       <div className="font-rsd-alt flex flex-col items-center p-5 text-center font-bold">
@@ -22,9 +34,15 @@ const BoardCard = ({ name, role }: BoardCardProps) => {
         <div className="flex text-center text-3xl text-white">{role}</div>
       </div>
       <div className="flex w-full flex-row place-items-end justify-end p-2">
-        <Mail className="flex h-12 w-12 text-white" />
-        <AiFillInstagram className="flex text-5xl text-white" />
-        <FaDiscord className="flex text-5xl text-white" />
+        <Link href={email}>
+          <Mail className="flex h-12 w-12 text-white" />
+        </Link>
+        <Link href={instagram}>
+          <AiFillInstagram className="flex text-5xl text-white" />
+        </Link>
+        <Link href={discord}>
+          <FaDiscord className="flex text-5xl text-white" />
+        </Link>
       </div>
     </div>
   );
