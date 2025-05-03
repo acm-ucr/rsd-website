0634e5e0de5f6f98bdbfbf008d95e721bbb633c1
stanleyx027 <sxiao027@ucr.edu>
2025-05-02 21:27:21 -0700

    Fix the needed changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Carousel from "@/components/home/GalleryPreview";
 import PracticeWrapper from "@/components/home/PracticeWrapper";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <PracticeWrapper />
+    <div className="flex h-1/2 w-screen flex-col items-center justify-center">
+      <div className="flex pb-100">
+        <PracticeWrapper />
+      </div>
+      <Carousel />
     </div>
   );
 };
--- src/components/home/PracticeWrapper.tsx
+++ src/components/home/PracticeWrapper.tsx
@@ -9,25 +9,22 @@ import Link from "next/link";
 const PracticeWrapper = () => {
   return (
     <div className="flex h-screen w-screen flex-col">
-      <div className="relative flex justify-center font-normal">
+      <div className="flex justify-center font-normal">
         <Header text="Practice Info" />
       </div>
       <Image src={Wave} alt="Wave" className="absolute z-0 flex w-full" />
-      <div className="relative z-10 flex flex-row items-start justify-evenly rounded-full">
+      <div className="z-10 flex flex-row items-start justify-evenly rounded-full">
         <div className="ml-5 flex w-3/4">
           <InfoBox
             text={
               <div className="text-left">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
               </div>
             }
           />
@@ -36,21 +33,19 @@ const PracticeWrapper = () => {
         <div className="mr-10 flex w-1/2">
           <InfoBox
             text={
-              <div className="p-4 text-left text-3xl">
-                <ul className="flex list-[square] flex-col items-start gap-5 text-center marker:text-3xl marker:text-white">
-                  <li>Sunscreen</li>
-                  <li>Water</li>
-                  <li>Change of clothes</li>
-                  <li>Open-toed shoes</li>
-                  <li>Towel</li>
-                </ul>
-              </div>
+              <ul className="flex list-[square] flex-col items-start gap-5 p-4 text-left text-3xl marker:text-3xl marker:text-white">
+                <li>Sunscreen</li>
+                <li>Water</li>
+                <li>Change of clothes</li>
+                <li>Open-toed shoes</li>
+                <li>Towel</li>
+              </ul>
             }
           />
         </div>
       </div>
 
-      <div className="relative z-10 flex flex-col items-center gap-30">
+      <div className="z-10 flex flex-col items-center gap-30">
         <PracticeShuttle />
         <Button className="bg-rsd-gold hover:bg-rsd-yellow font-rsd-alt rounded-full p-15 text-center text-5xl font-bold text-black">
           <Link href="/">more events!</Link>
