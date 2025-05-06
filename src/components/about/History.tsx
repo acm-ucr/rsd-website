9a9a46c5ad2ba9fb2a83cb013b5bf4384912591f
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-05-05 21:40:25 -0700

    added formatting and sorta have what is wanted

--- src/components/about/History.tsx
+++ src/components/about/History.tsx
@@ -13,14 +13,13 @@ interface ComponentProps {
 const History = ({ title, text }: ComponentProps) => {
   return (
     <div className="relative w-full h-3/4 flex flex-col items-center justify-center overflow-hidden">
-      <Image
-        src={background}
-        alt="Dragon Boat Background"
-        fill
-        className="object-cover absolute top-0 left-0 z-0"
-      />
-          
-      <div className="z-10 px-6 w-full flex flex-col items-center text-center">
+        <Image
+          src={background}
+          alt="Dragon Boat Background"
+          fill
+          className="object-cover translate-y-20 absolute top-0 left-0 z-0"
+        />  
+      <div className="z-10 px-6 py-14 w-full flex flex-col items-center text-center">
         <Header text={title} />
         <InfoBox text={text}/>
       </div>
