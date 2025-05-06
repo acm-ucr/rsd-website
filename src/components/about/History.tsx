16ab005b170f0be42cb51d2ff3e01652dc256a1a
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-05-06 09:26:03 -0700

    formatting

--- src/components/about/History.tsx
+++ src/components/about/History.tsx
@@ -1,30 +1,28 @@
-import Header from "@/components/Header"
-import InfoBox from "@/components/InfoBox"
+import Header from "@/components/Header";
+import InfoBox from "@/components/InfoBox";
 import Image from "next/image";
-import background from "@/public/about/dragon.webp"
+import background from "@/public/about/dragon.webp";
 
 interface ComponentProps {
   title: string;
   text: string;
-
 }
-
 
 const History = ({ title, text }: ComponentProps) => {
   return (
-    <div className="relative w-full h-3/4 flex flex-col items-center justify-center overflow-hidden">
-        <Image
-          src={background}
-          alt="Dragon Boat Background"
-          fill
-          className="object-cover translate-y-20 absolute top-0 left-0 z-0"
-        />  
-      <div className="z-10 px-6 py-14 w-full flex flex-col items-center text-center">
+    <div className="relative flex h-3/4 w-full flex-col items-center justify-center overflow-hidden">
+      <Image
+        src={background}
+        alt="Dragon Boat Background"
+        fill
+        className="absolute top-0 left-0 z-0 translate-y-20 object-cover"
+      />
+      <div className="z-10 flex w-full flex-col items-center px-6 py-14 text-center">
         <Header text={title} />
-        <InfoBox text={text}/>
+        <InfoBox text={text} />
       </div>
     </div>
-  )
+  );
 };
 
 export default History;
