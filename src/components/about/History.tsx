b010693ff87dc8590f465f6473bc9e0c677beb1b
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-05-05 17:20:45 -0700

    General shape of history component achieved, need to move the bg image

--- src/components/about/History.tsx
+++ src/components/about/History.tsx
@@ -1,5 +1,31 @@
-const History = () => {
-  return <div>History</div>;
+import Header from "@/components/Header"
+import InfoBox from "@/components/InfoBox"
+import Image from "next/image";
+import background from "@/public/about/dragon.webp"
+
+interface ComponentProps {
+  title: string;
+  text: string;
+
+}
+
+
+const History = ({ title, text }: ComponentProps) => {
+  return (
+    <div className="relative w-full h-3/4 flex flex-col items-center justify-center overflow-hidden">
+      <Image
+        src={background}
+        alt="Dragon Boat Background"
+        fill
+        className="object-cover absolute top-0 left-0 z-0"
+      />
+          
+      <div className="z-10 px-6 w-full flex flex-col items-center text-center">
+        <Header text={title} />
+        <InfoBox text={text}/>
+      </div>
+    </div>
+  )
 };
 
 export default History;
