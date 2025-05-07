1fa106fa0d9516040cc02d8e37312a99f5360f81
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-05-06 20:14:07 -0700

    fixed merge conflicts

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,11 @@
 import History from "@/components/about/History";
 import Race from "@/components/about/RaceResults";
-import Header from "@/components/Header";
 import ImportantEvents from "@/components/about/ImportantEvents";
 
 const About = () => {
   return (
     <div className="flex flex-col items-center">
       <ImportantEvents />
-      <Header text="History of Dragon Boat" />
-      <InfoBox text="Dragon boat racing dates back over 2,000 years to ancient China, where it originated as a ritual to honor the poet Qu Yuan and bring good fortune through competitive paddling. Over time, it evolved into an International water sport, blending tradition with high-intensity teamwork and endurance. At UCR, the Riverside Surging Dragons continues this legacy, providing students with a unique opportunity to build strength, camaraderie, and school spirit through competitive racing and community engagement." />
       <History />
       <Race />
     </div>
