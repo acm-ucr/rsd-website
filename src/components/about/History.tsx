362e20e4ca680294322174e292c1a317b1782b8a
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-05-06 19:19:10 -0700

    hard coded text for Header and InfoBox

--- src/components/about/History.tsx
+++ src/components/about/History.tsx
@@ -3,12 +3,7 @@ import InfoBox from "@/components/InfoBox";
 import Image from "next/image";
 import background from "@/public/about/dragon.webp";
 
-interface ComponentProps {
-  title: string;
-  text: string;
-}
-
-const History = ({ title, text }: ComponentProps) => {
+const History = () => {
   return (
     <div className="relative flex h-3/4 w-full flex-col items-center justify-center overflow-hidden">
       <Image
@@ -18,8 +13,8 @@ const History = ({ title, text }: ComponentProps) => {
         className="absolute top-0 left-0 z-0 translate-y-20 object-cover"
       />
       <div className="z-10 flex w-full flex-col items-center px-6 py-14 text-center">
-        <Header text={title} />
-        <InfoBox text={text} />
+        <Header text="History of Dragon Boat" />
+        <InfoBox text="Dragon boat racing dates back over 2,000 years to ancient China, where it originated as a ritual to honor the poet Qu Yuan and bring good fortune through competitive paddling. Over time, it evolved into an international water sport, blending tradition with high-intensity teamwork and endurance. At UCR, the Riverside Surging Dragons continues this legacy, providing students with a unique opportunity to build strength, camaraderie, and school spirit through competitive racing and community engagement." />
       </div>
     </div>
   );
