c8a3a6bae11bb3566134e76edf3d64f86fecda01
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-05-09 10:57:50 -0700

    Updated social media links

--- src/data/FooterLinks.ts
+++ src/data/FooterLinks.ts
@@ -1,12 +1,12 @@
 import { FaInstagram, FaDiscord, FaEnvelope, FaYoutube } from "react-icons/fa";
 
 export const footerLinks = [
-  { name: "Mail", icon: FaEnvelope, url: "mailto:sampleEmail@example.com" },
-  { name: "Discord", icon: FaDiscord, url: "https://discord.gg/aserver" },
+  { name: "Mail", icon: FaEnvelope, url: "mailto:rsd.dragonboat@gmail.com" },
+  { name: "Discord", icon: FaDiscord, url: "https://discord.gg/czUqmCBHnm" },
   {
     name: "Instagram",
     icon: FaInstagram,
-    url: "https://instagram.com/profile",
+    url: "https://instagram.com/rsd_dragonboat",
   },
-  { name: "Youtube", icon: FaYoutube, url: "https://youtube.com" },
+  { name: "Youtube", icon: FaYoutube, url: "https://youtube.com/@riversidesurgingdragons7640" },
 ];
