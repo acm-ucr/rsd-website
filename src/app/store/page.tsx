08efcca65b531d2b6dc735e5a1bf55c93deee5ea
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-05-09 14:59:30 -0700

    Completed 1. adding header for feature products and stickers? text

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,15 +1,18 @@
 import StickersBox from "@/components/store/StickersBox";
-
+import Header from "@/components/Header";
 import ProductCard from "@/components/store/ProductCard";
 
 const Store = () => {
   return (
     <div>
-      Store
-      <div className="row flex h-screen w-screen items-center justify-center">
-        <ProductCard productName="Shirt" productPrice="17.99$" />
-        <ProductCard productName="Jacket" productPrice="29.99$" />
+      <div className="flex h-screen w-screen flex-col items-center justify-center">
+        <Header text="Featured Products" />
+        <div className="flex h-screen w-screen items-center justify-center">
+          <ProductCard productName="Shirt" productPrice="17.99$" />
+          <ProductCard productName="Jacket" productPrice="29.99$" />
+        </div>
       </div>
+      <Header text="Stickers?" />
       <StickersBox />
     </div>
   );
