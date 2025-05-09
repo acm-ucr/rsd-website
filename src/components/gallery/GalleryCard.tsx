8ec34f7be01fc491ad5b1292db03671d6ce9627c
Nina <nina@ucr-secure-48-10-13-243-77.wnet.ucr.edu>
2025-05-09 09:54:21 -0700

    color change

--- src/components/gallery/GalleryCard.tsx
+++ src/components/gallery/GalleryCard.tsx
@@ -14,7 +14,7 @@ const GalleryCard = ({ photoName, link, image }: GalleryInfo) => {
       <div className="font-rsd-alt relative h-60 w-96 flex-col items-center rounded-t-2xl bg-gray-200">
         <Image className="object-cover" src={image} alt="product" fill />
       </div>
-      <div className="bg-rsd-light-blue rounded-b-2xl p-2 text-center text-xl">
+      <div className="bg-white rounded-b-2xl p-2 text-center text-xl">
         {photoName}
       </div>
     </Link>
