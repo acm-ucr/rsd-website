dd813ae8600e3ccc4b481b78bd4bdf9fad4e45e0
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-05-09 12:00:55 -0700

    fixed width and height to match the navbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,17 +7,15 @@ import { footerLinks } from "@/data/FooterLinks";
 const Footer = () => {
   return (
     <div className="bg-rsd-mid-blue flex items-center justify-between px-6 py-4">
-      <Link href="/" className="transition-opacity hover:opacity-80">
-        <Image src={footerImage} alt="RSD Logo" width={100} height={50} />
+      <Link href="/" className="transition-opacity hover:opacity-80 w-1/20">
+        <Image src={footerImage} alt="RSD Logo" />
       </Link>
-
       <div className="flex space-x-4">
         {footerLinks.map(({ icon: Icon, url }, idx) => (
           <Link
             key={idx}
             href={url}
             target="_blank"
-            rel="noopener noreferrer"
             className="transition-transform hover:scale-110"
           >
             <Icon className="h-8 w-8 text-white" />
