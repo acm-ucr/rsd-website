b1349e94cd28ad19d766d2895ab681df45fc48d4
ALE00111 <austintle56@gmail.com>
2025-05-09 00:24:43 -0700

    Updating carousel to fix positioning and overlapping issues of slides

--- src/components/home/GalleryPreview.tsx
+++ src/components/home/GalleryPreview.tsx
@@ -37,20 +37,25 @@ const GalleryPreview = () => {
         className="w-[90vw]"
       >
         <CarouselPrevious />
-        <CarouselContent className = "w-[95vw] relative">
+        <CarouselContent className="relative w-[95vw]">
           {slides.map((slide: slide, index: number) => (
             <CarouselItem
               key={slide.id}
-              className={`lg:basis-1/3
-                        ${(current === (index - 1 + totalSlides) % totalSlides || 
-                          current === (index + 1) % totalSlides || 
-                          current === index) ? "visible flex justify-center" : "invisible"}
-                          `}>
-                <Card className ={`bg-rsd-yellow w-11/12 ${current === index ? "scale-[100%]" : "scale-[75%]"}`}>
-                  <CardContent className="flex aspect-square items-center justify-center text-6xl">
-                    {slide.id}
-                  </CardContent>
-                </Card>
+              className={`lg:basis-1/3 ${
+                current === (index - 1 + totalSlides) % totalSlides ||
+                current === (index + 1) % totalSlides ||
+                current === index
+                  ? "visible flex justify-center"
+                  : "invisible"
+              } `}
+            >
+              <Card
+                className={`bg-rsd-yellow w-11/12 ${current === index ? "scale-[100%]" : "scale-[75%]"}`}
+              >
+                <CardContent className="flex aspect-square items-center justify-center text-6xl">
+                  {slide.id}
+                </CardContent>
+              </Card>
             </CarouselItem>
           ))}
         </CarouselContent>
