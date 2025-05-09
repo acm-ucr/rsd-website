badd76a71d9fb48252466f05913a7a9e795f0218
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-05-09 15:50:38 -0700

    Added a non-functional tiny button that needs a lot of work and the background mentioned in 3.

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,19 +1,30 @@
 import StickersBox from "@/components/store/StickersBox";
 import Header from "@/components/Header";
 import ProductCard from "@/components/store/ProductCard";
+import { Button } from "@/components/ui/button"
+import Image from "next/image"; 
+import storeWave from "@/public/store/storeWave.webp"
 
 const Store = () => {
   return (
     <div>
-      <div className="flex w-screen flex-col items-center justify-center">
+      <div className="relative flex w-screen flex-col items-center justify-center pb-43">
         <Header text="Featured Products" />
         <div className="flex items-center justify-center">
           <ProductCard productName="Shirt" productPrice="17.99$" />
           <ProductCard productName="Jacket" productPrice="29.99$" />
         </div>
+        <Image
+          src={storeWave}
+          alt="store wave background"
+          className="absolute bottom-0 left-0 z-0 w-full object-cover"
+        />
+        <Button />
       </div>
-      <Header text="Stickers?" />
-      <StickersBox />
+      <div>
+        <Header text="Stickers?" />
+        <StickersBox />
+      </div>
     </div>
   );
 };
