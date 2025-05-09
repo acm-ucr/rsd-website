61001a68e145e307c163dc761dd3fef0cfe50791
Nina <nina@ucr-secure-48-10-13-243-77.wnet.ucr.edu>
2025-05-09 09:48:42 -0700

    stopped image stretching

--- src/components/gallery/GalleryCard.tsx
+++ src/components/gallery/GalleryCard.tsx
@@ -1,26 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
+import { StaticImageData } from "next/image"
 
 interface GalleryInfo {
   photoName: string;
   link: string;
-  image: string;
+  image: string | StaticImageData;
 }
 
 const GalleryCard = ({ photoName, link, image }: GalleryInfo) => {
   return (
     <Link href={link} target="_blank">
-      <div className="font-rsd-alt h-48 w-96 min-w-0 flex-col items-center rounded-t-2xl bg-gray-200">
-        <Image
-          className="h-full w-full"
+      <div className="relative font-rsd-alt h-60 w-96 flex-col items-center rounded-t-2xl bg-gray-200">
+      <Image
+          className="object-cover"
           src={image}
           alt="product"
-          width={400}
-          height={400}
+          fill
         />
-        <div className="rounded-b-2xl bg-white p-2 text-center text-xl">
-          {photoName}
         </div>
+        <div className="rounded-b-2xl bg-rsd-light-blue p-2 text-center text-xl">
+          {photoName}
       </div>
     </Link>
   );
