a79e3da40f2b0ce82306b45a70d3edeb75f65ea6
Asir Mazhar <asirmazhar23@gmail.com>
2025-05-10 11:09:33 -0700

    landing page (needs help)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 //import Example from "@/components/Example";
 import Carousel from "@/components/home/GalleryPreview";
+import Landing from "@/components/home/Landing";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="bg-rsd-light-blue flex h-screen w-screen flex-col items-center justify-center">
       {/* <Example text1="Adjust" text2="these" text3="Props" /> */}
+      <Landing />
       <Carousel />
     </div>
   );
--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -1,5 +1,33 @@
+import Image from "next/image";
+import dragon from "@/public/home/dragon.webp";
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div className="bg-rsd-mid-blue relative w-[50%] rounded-4xl p-3 text-center">
+      <Image
+        src={dragon}
+        alt="dragon outline"
+        //add tailwind classes for the image
+        //mask clip for edges
+        className="bg-rsd-dark-blue rounded-4xl object-contain"
+      />
+
+      {/* //stars go here and i just eyeball that shi (place a img tag and place em top bot left right) */}
+
+      <div className="item-center absolute inset-0 inset-x-1/4 inset-y-2/7 flex flex-col text-center">
+        <p className="font-rsd-main text-rsd-yellow text-center text-4xl">
+          Riverside <br /> Surging Dragons
+        </p>
+        <br />
+        <p className="font-rsd-alt item-center text-center text-lg text-white">
+          Riverside Surging Dragons is a dragon boat club at UCR, rooted in the
+          traditions of Chinese sport: dragon boat racing. We are a devoted team
+          of college students, eager to thrive in the water and build
+          everlasting friendships whilst fostering an inclusive and welcoming
+          environment.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
