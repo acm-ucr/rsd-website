223f60c49b3ffe9fe09f4514698cd811a9319e2c
Willow <157853295+mindisaias@users.noreply.github.com>
2025-05-11 16:59:42 -0700

    Update src/components/store/ProductCard.tsx
    Co-authored-by: Quin Gill <152361186+qhgill@users.noreply.github.com>

--- src/components/store/ProductCard.tsx
+++ src/components/store/ProductCard.tsx
@@ -10,7 +10,7 @@ const ProductCard = ({ productName, productPrice }: ProductTypes) => {
   return (
     <div className="font-rsd-alt flex-col items-center p-10 text-xl">
       <div className="flex h-145 w-125 min-w-0 items-center justify-center rounded-2xl bg-gray-200">
-        <Image className="max-h-145 max-w-125" src={product} alt="product" />
+        <Image className="object-cover" src={product} alt="product" />
       </div>
       <div className="p-2 text-center text-3xl">{productName}</div>
       <div className="text-center text-2xl">{productPrice}</div>
