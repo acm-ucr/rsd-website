da1dfa6798e2d8ce10b1a561296fc79efd256922
Isaias Villanueva <157853295+mindisaias@users.noreply.github.com>
2025-05-11 17:12:17 -0700

    added margin to bottom of page and put button + bg images into own relative div

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -10,33 +10,35 @@ import Link from "next/link";
 const Store = () => {
   return (
     <div>
-      <div className="relative flex w-screen flex-col items-center justify-center pb-47">
+      <div className="relative flex w-screen flex-col items-center justify-center">
         <Header text="Featured Products" />
         <div className="flex items-center justify-center">
           <ProductCard productName="Shirt" productPrice="17.99$" />
           <ProductCard productName="Jacket" productPrice="29.99$" />
         </div>
-        <Image
-          src={storeWave}
-          alt="store wave background"
-          className="absolute bottom-4 left-0 -z-10 w-full object-cover"
-        />
-        <Image
-          src={Cloud}
-          alt="store wave background"
-          className="absolute bottom-20 -left-25 -z-10 h-1/8 w-1/5"
-        />
-        <Image
-          src={Cloud}
-          alt="store wave background"
-          className="absolute -right-20 bottom-83 -z-10 w-1/6 scale-x-[-1]"
-        />
-        <Button className="font-rsd-alt rounded-full bg-[#485D8A] p-15 text-center text-5xl font-bold text-white">
-          <Link href="/">order form!</Link>
-          {/* Could not find a current link to merch, only old link */}
-        </Button>
+        <div className="relative flex w-screen items-center justify-center pt-7.5 pb-47">
+          <Image
+            src={storeWave}
+            alt="store wave background"
+            className="absolute bottom-4 left-0 -z-10 w-full object-cover"
+          />
+          <Image
+            src={Cloud}
+            alt="store wave background"
+            className="absolute bottom-20 -left-25 -z-10 h-1/2 w-1/5"
+          />
+          <Image
+            src={Cloud}
+            alt="store wave background"
+            className="absolute -right-20 bottom-83 -z-10 w-1/6 scale-x-[-1]"
+          />
+          <Button className="font-rsd-alt rounded-full bg-[#485D8A] p-15 text-center text-5xl font-bold text-white">
+            <Link href="/">order form!</Link>
+            {/* Could not find a current link to merch, only old link */}
+          </Button>
+        </div>
       </div>
-      <div className="relative">
+      <div className="relative m-25">
         <Header text="Stickers?" />
         <StickersBox />
       </div>
