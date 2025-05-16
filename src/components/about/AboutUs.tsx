4449ef9a12be3d2f4c3e6f102baa14861ca81c9a
Nina <nina@ucr-secure-38-10-13-201-226.wnet.ucr.edu>
2025-05-16 11:26:50 -0700

    minor formatting of classname

--- src/components/about/AboutUs.tsx
+++ src/components/about/AboutUs.tsx
@@ -12,15 +12,21 @@ const AboutUs = () => {
         text="Dragon Boat is a team sport with 18-20 paddlers, a drummer, and a steer. The drummer sits at the front, drumming to set the pace and make calls when necessary. The steer stands at the back of the boat (the stern) and guides the boat through the water, keeping it on course. 
     Riverside Surging Dragons is UCR’s competitive dragon boat team, where competition meets community! We push each other to be stronger on the water while creating a tight-knit community through team socials, bonding events, and shared experiences. Whether you’re here to compete, make friends, or try something new, our boat is always open to you!"
       />
-      <div className="absolute top-60 left-20 rotate-45">
-        <Image src={Star4Point} alt="Four Point Star" width={130} />
-      </div>
-      <div className="absolute top-75 left-10">
-        <Image src={Star4Point} alt="Four Point Star" width={80} />
-      </div>
-      <div className="absolute top-8 -right-40 scale-110 scale-x-[-1] -rotate-35">
-        <Image src={Paddles} alt="Paddles" />
-      </div>
+      <Image
+        src={Star4Point}
+        alt="Four Point Star"
+        className="absolute top-60 left-25 w-30 rotate-45"
+      />
+      <Image
+        src={Star4Point}
+        alt="Four Point Star"
+        className="absolute top-75 left-10 w-20"
+      />
+      <Image
+        src={Paddles}
+        alt="Paddles"
+        className="absolute top-8 -right-40 scale-110 scale-x-[-1] -rotate-35"
+      />
     </div>
   );
 };
