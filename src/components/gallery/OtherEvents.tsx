e5d409ae45ff13d21ed3d29968155902e829fa27
LawMot <molawr12@gmail.com>
2025-05-16 18:33:08 -0700

    created other events

--- src/components/gallery/OtherEvents.tsx
+++ src/components/gallery/OtherEvents.tsx
@@ -1,5 +1,23 @@
+import GalleryCard from "./GalleryCard";
+import Image from "next/image";
+import wave from "@/public/wave.webp";
+import logo from "@/public/RSDLogo.webp";
+
 const OtherEvents = () => {
-  return <div>OtherEvents</div>;
+  return (
+    <div className="relative pb-10">
+      <Image src={wave} alt="wave" className="absolute bottom-0 w-screen" />
+      <div className="relative z-10">
+        <div className="text-rsd-button-blue font-rsd-alt flex justify-center pb-10 text-3xl font-bold">
+          Other Events
+        </div>
+        <div className="flex items-center justify-center gap-15 pb-10">
+          <GalleryCard photoName="Practices ‘24-’25" link="/" image={logo} />
+          <GalleryCard photoName="Socials ‘24-’25" link="/" image={logo} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OtherEvents;
