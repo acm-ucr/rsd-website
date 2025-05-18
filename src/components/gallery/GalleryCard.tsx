1b01cfdbcb8b4cc02f03c748c9b0d7ae70b0828a
LawMot <molawr12@gmail.com>
2025-05-17 22:01:21 -0700

    ran format

--- src/components/gallery/GalleryCard.tsx
+++ src/components/gallery/GalleryCard.tsx
@@ -10,17 +10,17 @@ interface GalleryInfo {
 
 const GalleryCard = ({ photoName, link, image }: GalleryInfo) => {
   return (
-    <div className="w-2/7 font-rsd-alt items-center">
-    <Link href={link} target="_blank">
-      <div className="w-full flex flex-col">
-      <div className="relative h-65 rounded-t-2xl bg-gray-200">
-        <Image className="object-cover" src={image} alt="product" fill />
-      </div>
-      <div className="rounded-b-2xl bg-white p-2 text-center text-xl">
-        {photoName}
-      </div>
-      </div>
-    </Link>
+    <div className="font-rsd-alt w-2/7 items-center">
+      <Link href={link} target="_blank">
+        <div className="flex w-full flex-col">
+          <div className="relative h-65 rounded-t-2xl bg-gray-200">
+            <Image className="object-cover" src={image} alt="product" fill />
+          </div>
+          <div className="rounded-b-2xl bg-white p-2 text-center text-xl">
+            {photoName}
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
