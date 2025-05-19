441319850ef22deead78bab5d7172e9c724190ff
qhgill <qhgill2@gmail.com>
2025-05-19 14:22:24 -0700

    tiny change

--- src/components/home/AwardsMapped.tsx
+++ src/components/home/AwardsMapped.tsx
@@ -4,10 +4,8 @@ import Header from "../Header";
 const AwardsMapped = () => {
   return (
     <div className="flex flex-col items-center">
-      <div>
-        <Header text="Awards and Recognition" />
-      </div>
-      <div className="mb-40 grid grid-cols-8 justify-center gap-5">
+      <Header text="Awards and Recognition" />
+      <div className="mb-40 grid grid-cols-4 justify-center gap-5">
         {Award.map((award, index) => (
           <AwardCard key={index} AwardName={award.name} />
         ))}
