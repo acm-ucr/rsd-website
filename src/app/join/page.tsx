c8f76dee761976310f69888e56d58584aa4f4ac0
stanleyx027 <sxiao027@ucr.edu>
2025-05-19 17:15:58 -0700

    Fix formatting for join's page.tsx and infobox.tsx

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -12,20 +12,20 @@ const Join = () => {
     <div className="bg-rsd-light-blue overflow-x-hidden">
       <Header text="Registration" />
       <div className="relative my-10 flex justify-center">
-        <div className="2xl:scale-100 invisible absolute -top-[10vh] -left-[16vw] sm:visible sm:-top-[12vh] sm:-left-[35vw] sm:scale-70 lg:-top-[12vh] lg:-left-[25vw] lg:scale-80 xl:scale-90 xl:-left-[16vw] 2xl:-top-[10vh]">
+        <div className="invisible absolute -top-[10vh] -left-[16vw] sm:visible sm:-top-[12vh] sm:-left-[35vw] sm:scale-70 lg:-top-[12vh] lg:-left-[25vw] lg:scale-80 xl:-left-[16vw] xl:scale-90 2xl:-top-[10vh] 2xl:scale-100">
           <Image src={Cloud} alt="Cloud" />
         </div>
         <InfoBox
           text={
             <div className="flex flex-col items-center justify-center text-center">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              <Button className="bg-rsd-mid-blue hover:bg-rsd-title-blue mt-5 rounded-4xl p-7 text-lg sm:text-xl mid:text-xl xl:p-8 xl:text-3xl">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              <Button className="bg-rsd-mid-blue hover:bg-rsd-title-blue mid:text-xl mt-5 rounded-4xl p-7 text-lg sm:text-xl xl:p-8 xl:text-3xl">
                 <Link href="/">Join Our Discord!</Link>
               </Button>
             </div>
