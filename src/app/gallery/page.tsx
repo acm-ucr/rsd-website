c6760a42fbb99e864afcea73d1b0cdb9e189abbc
Adam Schumperli <aschu042@ucr.edu>
2025-05-20 17:09:15 -0700

    finished the QuarterlyRaces file with a subtitle both images and the gallery cards using absolute positioning

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -4,9 +4,9 @@ import Header from "@/components/Header";
 const Gallery = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center">
-      <Header text="Gallery"/>
-      <QuarterlyRaces/>
-      <OtherEvents/>
+      <Header text="Gallery" />
+      <QuarterlyRaces />
+      <OtherEvents />
     </div>
   );
 };
