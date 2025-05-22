9a37ca0278011c082b9684fa6d7397d431ae9c4c
qhgill <qhgill2@gmail.com>
2025-05-22 14:15:13 -0700

    home page fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,10 @@ const Home = () => {
   return (
     <div className="flex h-1/2 w-screen flex-col items-center justify-center">
       <Landing />
-      <div className="mt-10 mb-10 flex w-3/5 items-center justify-center">
-        <iframe
-          src="https://www.youtube.com/embed/Xs0hR3QIQNE?si=qkDfhaD82i5ogqoe"
-          className="mt-10 mb-10 aspect-video w-full rounded-xl md:w-3/4 lg:w-2/3"
-        />
-      </div>
+      <iframe
+        src="https://www.youtube.com/embed/Xs0hR3QIQNE?si=qkDfhaD82i5ogqoe"
+        className="mt-10 mb-10 aspect-video w-full rounded-xl md:w-3/4 lg:w-2/3"
+      />
       <AwardsMapped />
       <div className="flex pb-100">
         <PracticeWrapper />
