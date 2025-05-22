3314f5166fd191b59c78bd2ba308cf6a20af8840
Adam Schumperli <aschu042@ucr.edu>
2025-05-22 13:35:47 -0700

    finished the QuarterlyRaces file with a subtitle both images and the gallery cards using absolute positioning

--- src/components/gallery/QuarterlyRaces.tsx
+++ src/components/gallery/QuarterlyRaces.tsx
@@ -5,39 +5,33 @@ import Image from "next/image";
 
 const QuarterlyRaces = () => {
   return (
-    <div className="relative w-full pb-[1100px]">
-      {/* Subtitle */}
+    <div className="relative w-full">
       <div className="text-rsd-button-blue font-rsd-alt flex justify-center pb-10 text-5xl font-bold">
         Quarterly Races
       </div>
 
-      {/* Image Assets */}
       <Image
         src={Dragon}
         alt="dragon"
-        className="absolute top-80 left-0 w-full"
+        className="absolute top-80 left-0 -z-10 w-full"
       />
       <Image
         src={Paddle}
         alt="double paddle"
-        className="absolute -top-40 left-30 w-145"
+        className="absolute -top-40 left-30 -z-10 w-145"
       />
 
-      {/* Gallery Cards */}
-
-      <div className="absolute top-24 right-20 w-1/2">
-        <div className="w-650">
-          <GalleryCard photoName="Fall '24" link="" image={Paddle} />
-        </div>
-      </div>
-      <div className="absolute top-112 left-20 w-1/2">
-        <div className="w-650">
-          <GalleryCard photoName="Winter '25" link="" image={Paddle} />
-        </div>
-      </div>
-      <div className="absolute top-200 right-20 w-1/2">
-        <div className="w-650">
-          <GalleryCard photoName="Spring '25" link="" image={Paddle} />
+      <div className="relative z-10 flex justify-center">
+        <div className="grid grid-cols-1 gap-12 px-20 py-2">
+          <div className="w-955 px-151">
+            <GalleryCard photoName="Fall '24" link="" image={Paddle} />
+          </div>
+          <div className="w-650">
+            <GalleryCard photoName="Winter '25" link="" image={Paddle} />
+          </div>
+          <div className="w-955 px-151">
+            <GalleryCard photoName="Spring '25" link="" image={Paddle} />
+          </div>
         </div>
       </div>
     </div>
