7df9c9a95afdca58277a5847a4c20293696613aa
LawMot <molawr12@gmail.com>
2025-05-22 23:21:02 -0700

    mobile responsive to featured products

--- src/components/store/ProductCard.tsx
+++ src/components/store/ProductCard.tsx
@@ -8,12 +8,14 @@ interface ProductTypes {
 
 const ProductCard = ({ productName, productPrice }: ProductTypes) => {
   return (
-    <div className="font-rsd-alt flex-col items-center p-10 text-xl">
-      <div className="flex h-145 w-125 min-w-0 items-center justify-center rounded-2xl bg-gray-200">
+    <div className="font-rsd-alt flex-col items-center p-2 text-xl md:p-5 lg:p-10">
+      <div className="flex h-43 w-37 min-w-0 items-center justify-center rounded-2xl bg-gray-200 md:h-87 md:w-75 lg:h-145 lg:w-125">
         <Image className="object-cover" src={product} alt="product" />
       </div>
-      <div className="p-2 text-center text-3xl">{productName}</div>
-      <div className="text-center text-2xl">{productPrice}</div>
+      <div className="text-center text-xl md:text-3xl lg:p-2">
+        {productName}
+      </div>
+      <div className="text-center text-lg md:text-2xl">{productPrice}</div>
     </div>
   );
 };
