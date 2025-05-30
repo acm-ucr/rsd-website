ee68a8db5c4dd15d1bef751ff8afe9c193db31c5
Nina S <n.m.sokolnikova06@gmail.com>
2025-05-30 09:51:57 -0700

    fade in and y shift animation for headers

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,22 @@
+"use client";
+import { motion } from "motion/react";
+
 interface HeaderProps {
   text: string;
 }
 const Header = ({ text }: HeaderProps) => {
   return (
-    <div className="mt-10 flex w-full items-center justify-center">
+    <motion.div
+      initial={{ opacity: 0.5, y: 10 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      viewport={{ once: true }}
+      className="mt-10 flex w-full items-center justify-center"
+    >
       <div className="font-rsd-main text-rsd-title-blue text-center text-5xl sm:text-6xl xl:text-7xl">
         {text}
       </div>
-    </div>
+    </motion.div>
   );
 };
 
