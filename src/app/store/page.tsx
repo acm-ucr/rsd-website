e4107e272d0b795e392919b991df51d5eaf9fef0
LawMot <molawr12@gmail.com>
2025-05-30 12:32:11 -0700

    change to md and overflow-hidden

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -10,13 +10,13 @@ import Link from "next/link";
 const Store = () => {
   return (
     <div>
-      <div className="relative flex w-screen flex-col items-center justify-center">
+      <div className="relative flex w-screen flex-col items-center justify-center overflow-hidden">
         <Header text="Featured Products" />
         <div className="flex flex-col items-center justify-center pt-4 md:flex-row md:pt-0">
           <ProductCard productName="Shirt" productPrice="17.99$" />
           <ProductCard productName="Jacket" productPrice="29.99$" />
         </div>
-        <div className="relative flex w-screen items-center justify-center pt-7.5 pb-25 md:pb-[22vh]">
+        <div className="relative flex w-screen items-center justify-center pt-7.5 md:pb-[22vh]">
           <Image
             src={storeWave}
             alt="store wave background"
@@ -25,12 +25,12 @@ const Store = () => {
           <Image
             src={Cloud}
             alt="store wave background"
-            className="absolute bottom-0 -left-32 -z-10 hidden w-7/10 lg:bottom-20 lg:-left-25 lg:block lg:w-1/5"
+            className="absolute bottom-0 -left-32 -z-10 hidden w-1/5 md:bottom-20 md:-left-25 md:block"
           />
           <Image
             src={Cloud}
             alt="store wave background"
-            className="absolute -right-32 bottom-0 -z-10 hidden w-7/10 scale-x-[-1] lg:-right-20 lg:bottom-83 lg:block lg:w-1/6"
+            className="absolute -right-32 bottom-0 -z-10 hidden w-1/6 scale-x-[-1] md:-right-20 md:bottom-60 md:block lg:bottom-83"
           />
           <Button className="font-rsd-alt bg-rsd-title-blue rounded-full p-10 text-center text-xl font-bold text-white hover:bg-blue-900 md:p-12 md:text-3xl lg:p-15 lg:text-5xl">
             <Link href="/">order form!</Link>
