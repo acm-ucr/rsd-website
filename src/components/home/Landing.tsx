ccc269fb39232eccfda219f4345f5092c757bab5
qhgill <qhgill2@gmail.com>
2025-05-30 23:12:00 -0700

    fix

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -5,44 +5,46 @@ import fiveStar from "@/public/5pointStar.webp";
 
 const Landing = () => {
   return (
-    <div className="bg-rsd-mid-blue relative m-10 flex h-1/2 w-full items-center justify-center rounded-4xl p-5 text-center sm:h-full md:w-13/15">
+    <div className="bg-rsd-mid-blue relative m-10 flex w-11/12 items-center justify-center rounded-4xl p-5 text-center sm:w-9/12 md:w-8/12 lg:rounded-[75px]">
+      <div className="bg-rsd-dark-blue rounded-4xl lg:rounded-[75px]">
+        <Image
+          src={dragon}
+          alt="dragon outline"
+          className="invisible rounded-4xl object-cover py-3 md:visible md:py-0 lg:rounded-[75px]"
+        />
+      </div>
       <Image
-        src={dragon}
-        alt="dragon outline"
-        className="bg-rsd-dark-blue h-[90vh] rounded-3xl object-cover sm:h-full lg:rounded-4xl"
+        src={fourStar}
+        alt="star"
+        className="absolute top-[18%] left-[21%] hidden size-10 sm:block lg:size-13 xl:size-16"
       />
       <Image
         src={fourStar}
         alt="star"
-        className="absolute top-[15vh] left-[10vh] size-10 sm:top-[10vh] sm:left-[10vh] md:left-[15vh] lg:top-[15vh] lg:left-[14vh] lg:size-13 xl:left-[16vh] xl:size-16"
-      />
-      <Image
-        src={fourStar}
-        alt="star"
-        className="absolute right-[10vh] bottom-[22vh] size-10 sm:right-[13vh] sm:bottom-[12vh] md:right-[11vh] md:bottom-[13vh] lg:right-[15vh] lg:bottom-[17vh] lg:size-13 xl:bottom-[20vh] xl:size-16"
+        className="absolute right-[25%] bottom-[10%] hidden size-10 sm:block md:bottom-[16%] lg:size-13 xl:size-16"
       />
       <Image
         src={fiveStar}
         alt="star"
-        className="absolute bottom-[14vh] left-[11vh] hidden size-7 sm:block md:left-[13vh] lg:bottom-[20vh] lg:left-[15vh] lg:size-9 xl:left-[18vh] xl:size-12"
+        className="absolute bottom-[13%] left-[20%] hidden size-7 sm:block md:bottom-[18%] lg:size-9 xl:size-12"
       />
       <Image
         src={fiveStar}
         alt="star"
-        className="absolute top-[30vh] left-[20vh] hidden size-7 sm:block md:top-[27vh] lg:top-[39vh] lg:left-[30vh] lg:size-9 xl:top-[45vh] xl:size-12"
+        className="absolute top-[50%] left-[8%] hidden size-7 lg:block lg:size-9 xl:size-12"
       />
       <Image
         src={fiveStar}
         alt="star"
-        className="absolute top-[25vh] right-[5vh] hidden size-7 sm:block md:top-[27vh] lg:top-[35vh] lg:size-9 xl:top-[42vh] xl:size-12"
+        className="absolute top-[50%] right-[10%] hidden size-7 lg:block lg:size-9 xl:size-12"
       />
 
-      <div className="absolute top-[20vh] flex w-2/3 flex-col items-center justify-center sm:top-[26vh] md:top-[10vh] md:h-9/12 lg:top-[18vh] lg:p-10 xl:w-3/4">
-        <div className="font-rsd-main text-rsd-yellow text-center text-2xl font-bold md:text-3xl lg:text-4xl xl:text-5xl">
+      <div className="absolute inset-y-0 flex w-4/5 flex-col items-center justify-center md:pt-4 lg:w-2/3">
+        <div className="font-rsd-main text-rsd-yellow text-center text-xl font-bold sm:text-2xl lg:text-4xl xl:text-5xl">
           Riverside <br />
           Surging Dragons
         </div>
-        <div className="font-rsd-alt item-center pt-2 text-center text-base text-white sm:text-sm md:text-base lg:text-xl xl:pt-4 xl:text-2xl 2xl:text-3xl">
+        <div className="font-rsd-alt item-center pt-2 text-center text-sm text-white sm:text-lg md:text-base xl:pt-4 xl:text-xl 2xl:text-2xl">
           Riverside Surging Dragons is a dragon boat club at UCR, rooted in the
           traditions of Chinese sport: dragon boat racing. We are a devoted team
           of college students, eager to thrive in the water and build
