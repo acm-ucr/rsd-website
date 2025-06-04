d7ec160c121f17881de5f9733ba846d6d889e3f9
stanleyx027 <sxiao027@ucr.edu>
2025-06-04 01:24:17 -0700

    Added animation to Board Card and Board Mapping

--- src/components/board/BoardMapping.tsx
+++ src/components/board/BoardMapping.tsx
@@ -1,6 +1,13 @@
+"use client";
 import boardMembers from "@/data/BoardCards";
 import BoardCard from "@/components/board/BoardCard";
 import Header from "@/components/Header";
+import { motion } from "motion/react";
+
+const boardAnimation = {
+  hidden: { opacity: 0, y: -50 },
+  display: { opacity: 1, y: 0 },
+};
 
 const BoardMapping = () => {
   return (
@@ -8,14 +15,22 @@ const BoardMapping = () => {
       <Header text="Meet the Board!" />
       <div className="mt-10 grid grid-cols-1 gap-x-40 gap-y-20 md:grid-cols-2">
         {boardMembers.map((member, index) => (
-          <BoardCard
-            key={index}
-            name={member.name}
-            role={member.title}
-            image={member.image}
-            email={member.email}
-            instagram={member.instagram}
-          />
+          <motion.div
+            variants={boardAnimation}
+            initial="hidden"
+            whileInView="display"
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true, amount: 0.3 }}
+          >
+            <BoardCard
+              key={index}
+              name={member.name}
+              role={member.title}
+              image={member.image}
+              email={member.email}
+              instagram={member.instagram}
+            />
+          </motion.div>
         ))}
       </div>
     </div>
