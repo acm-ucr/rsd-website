bee773973f57018f7a7f47a49bd399b335aec936
Willow <isaiasnoel@gmail.com>
2025-06-05 15:52:11 -0700

    animated merch product card, assets, and stickerbox

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import StickersBox from "@/components/store/StickersBox";
 import Header from "@/components/Header";
 import ProductCard from "@/components/store/ProductCard";
@@ -6,17 +7,30 @@ import Image from "next/image";
 import storeWave from "@/public/store/storeWave.webp";
 import Cloud from "@/public/cloud.webp";
 import Link from "next/link";
+import { motion } from "motion/react";
 
 const Store = () => {
   return (
     <div>
       <div className="relative flex w-screen flex-col items-center justify-center overflow-hidden">
         <Header text="Featured Products" />
-        <div className="flex flex-col items-center justify-center pt-4 md:flex-row md:pt-0">
+        <motion.div
+          transition={{ duration: 0.5 }}
+          initial={{ opacity: 0.3, y: 10 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          className="flex flex-col items-center justify-center pt-4 md:flex-row md:pt-0"
+        >
           <ProductCard productName="Shirt" productPrice="$17.99" />
           <ProductCard productName="Jacket" productPrice="$29.99" />
-        </div>
-        <div className="relative flex w-screen items-center justify-center pt-7.5 md:pb-[22vh]">
+        </motion.div>
+        <motion.div
+          transition={{ duration: 0.5 }}
+          initial={{ opacity: 0.3, y: 10 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          className="relative flex w-screen items-center justify-center pt-7.5 md:pb-[22vh]"
+        >
           <Image
             src={storeWave}
             alt="store wave background"
@@ -35,11 +49,18 @@ const Store = () => {
           <Button className="font-rsd-alt bg-rsd-title-blue rounded-full p-10 text-center text-xl font-bold text-white hover:bg-blue-900 md:p-12 md:text-3xl lg:p-15 lg:text-5xl">
             <Link href="/">order form!</Link>
           </Button>
-        </div>
+        </motion.div>
       </div>
       <div className="relative m-10 flex flex-col items-center md:m-25">
         <Header text="Stickers?" />
-        <StickersBox />
+        <motion.div
+          transition={{ duration: 0.5 }}
+          initial={{ opacity: 0.3, y: 10 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+        >
+          <StickersBox />
+        </motion.div>
       </div>
     </div>
   );
