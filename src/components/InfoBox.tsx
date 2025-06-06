03f4fe973b6d4d8358e1f3c49e03ddb919f8d926
Asir Mazhar <asirmazhar23@gmail.com>
2025-06-06 11:27:00 -0700

    added animation to info box

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,13 +1,25 @@
+"use client";
+import { motion } from "motion/react";
 interface InfoBoxProps {
   text: React.ReactNode;
 }
+
+const Animation = {
+  initial: { opacity: 0, y: 15 },
+  whileInView: { opacity: 1, y: 0 },
+  transition: { duration: 0.5 },
+  className: "m-10 flex w-full max-w-screen items-center justify-center",
+};
+
 const InfoBox: React.FC<InfoBoxProps> = ({ text }) => {
   return (
-    <div className="my-10 w-11/12 px-2 text-left md:my-12 xl:px-8">
-      <div className="font-rsd-alt bg-rsd-dark-blue flex h-full justify-center rounded-4xl px-16 py-8 text-base font-bold text-white lg:px-20 lg:py-10 lg:text-2xl 2xl:text-3xl">
-        {text}
+    <motion.div {...Animation}>
+      <div className="my-10 w-11/12 px-2 text-left md:my-12 xl:px-8">
+        <div className="font-rsd-alt bg-rsd-dark-blue flex h-full justify-center rounded-4xl px-16 py-8 text-base font-bold text-white lg:px-20 lg:py-10 lg:text-2xl 2xl:text-3xl">
+          {text}
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
