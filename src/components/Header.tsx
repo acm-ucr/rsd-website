628e215c8f511f0c5e66714b5a0d6724c99fd651
ALE00111 <austintle56@gmail.com>
2025-06-12 10:24:06 -0700

    Fixing merge conflict

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const wrapAnimation = {
 
 const Header = ({ text }: HeaderProps) => {
   return (
-    <motion.div {...wrapAnimation} className="flex justify-center py-4">
+    <motion.div {...wrapAnimation}>
       <div className="font-rsd-main text-rsd-title-blue text-center text-5xl sm:text-6xl xl:text-7xl">
         {text}
       </div>
--- src/components/home/Video.tsx
+++ src/components/home/Video.tsx
@@ -1,12 +1,25 @@
+import { motion } from "motion/react";
+
+const animationFadeIn = {
+  initial: { opacity: 0, y: 10 },
+  whileInView: { opacity: 1, y: 0 },
+};
+
 const Video = () => {
   return (
     <div className="flex justify-center">
-      <div className="mt-10 mb-10 w-5/6 max-w-screen-lg">
+      <motion.div
+        variants={animationFadeIn}
+        initial="initial"
+        whileInView="whileInView"
+        transition={{ duration: 0.7, delay: 0.4 }}
+        className="mt-10 mb-10 flex w-5/6 max-w-screen-lg items-center justify-center px-5"
+      >
         <iframe
           src="https://www.youtube.com/embed/Xs0hR3QIQNE?si=qkDfhaD82i5ogqoe"
           className="mt-5 mb-5 aspect-video w-full rounded-xl"
         />
-      </div>
+      </motion.div>
     </div>
   );
 };
