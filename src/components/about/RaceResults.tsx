92ec459002300e589ab80d9b6f19c6518f670564
ALE00111 <austintle56@gmail.com>
2025-06-15 18:19:52 -0700

    fixing merge conflict

--- src/components/about/RaceResults.tsx
+++ src/components/about/RaceResults.tsx
@@ -20,39 +20,39 @@ const RaceResults = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center">
       <Header text="List of Race Results" />
-         <motion.div
+      <motion.div
         variants={carouselAnimation}
         initial="initial"
         whileInView="whileInView"
         transition={{ duration: 0.5 }}
       >
-      <div className="bg-rsd-dark-blue mb-10 flex w-[60vw] flex-col items-center rounded-4xl px-5 py-15">
-        <Carousel
-          opts={{
-            loop: false,
-          }}
-          className="w-[60vw]"
-        >
-          <CarouselContent>
-            {slides.map(({ year, title, subtitle }, index) => (
-              <CarouselItem key={index} className="md:basis-1/3">
-                <Card className="bg-rsd-dark-blue border-none shadow-none">
-                  <CardContent className="font-rsd-alt flex aspect-square flex-col items-center justify-center font-bold">
-                    <span className="text-rsd-yellow text-3xl">{year}</span>
-                    <span className="text-lg text-white">{title}</span>
-                    <span className="text-rsd-light-blue text-xs">
-                      {subtitle}
-                    </span>
-                  </CardContent>
-                </Card>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious className="text-rsd-light-blue absolute left-2 bg-transparent" />
-          <CarouselNext className="text-rsd-light-blue absolute right-2 bg-transparent" />
-        </Carousel>
-      </div>
-</motion.div>
+        <div className="bg-rsd-dark-blue mb-10 flex w-[60vw] flex-col items-center rounded-4xl px-5 py-15">
+          <Carousel
+            opts={{
+              loop: false,
+            }}
+            className="w-[60vw]"
+          >
+            <CarouselContent>
+              {slides.map(({ year, title, subtitle }, index) => (
+                <CarouselItem key={index} className="md:basis-1/3">
+                  <Card className="bg-rsd-dark-blue border-none shadow-none">
+                    <CardContent className="font-rsd-alt flex aspect-square flex-col items-center justify-center font-bold">
+                      <span className="text-rsd-yellow text-3xl">{year}</span>
+                      <span className="text-lg text-white">{title}</span>
+                      <span className="text-rsd-light-blue text-xs">
+                        {subtitle}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious className="text-rsd-light-blue absolute left-2 bg-transparent" />
+            <CarouselNext className="text-rsd-light-blue absolute right-2 bg-transparent" />
+          </Carousel>
+        </div>
+      </motion.div>
     </div>
   );
 };
