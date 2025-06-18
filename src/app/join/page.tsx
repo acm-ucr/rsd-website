3e65a45326bbc374a7bff7512457d44bf2be0eb1
stanleyx027 <sxiao027@ucr.edu>
2025-06-18 14:35:38 -0700

    Added animation to join page assets

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import JoinDragon from "@/public/join/joinDragon.webp";
 import Cloud from "@/public/cloud.webp";
@@ -6,17 +7,22 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Registration from "@/components/join/Registration";
 import Header from "@/components/Header";
+import { motion } from "motion/react";
 
 const Join = () => {
   return (
     <div className="bg-rsd-light-blue flex min-h-screen w-full flex-col items-center overflow-x-hidden">
       <Header text="Registration" />
+
       <div className="relative flex justify-center">
-        <Image
-          src={Cloud}
-          alt="Cloud"
-          className="invisible absolute -top-[10vh] -left-[16vw] sm:visible sm:-top-[12vh] sm:-left-[35vw] sm:scale-70 md:-top-[10vh] md:scale-80 lg:-top-[12vh] lg:-left-[25vw] xl:-left-[16vw] xl:scale-90 2xl:-top-[10vh] 2xl:scale-100"
-        />
+        <motion.div
+          initial={{ opacity: 0, x: -10 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          className=":sm:-top-[12vh] invisible absolute -top-[10vh] -left-[16vw] sm:visible sm:-left-[35vw] sm:scale-70 md:-top-[10vh] md:scale-80 lg:-top-[12vh] lg:-left-[25vw] xl:-left-[16vw] xl:scale-90 2xl:-top-[10vh] 2xl:scale-100"
+        >
+          <Image src={Cloud} alt="Cloud" />
+        </motion.div>
 
         <InfoBox
           text={
@@ -30,19 +36,30 @@ const Join = () => {
             </div>
           }
         />
-        <Image
-          src={Cloud}
-          alt="Cloud"
+        <motion.div
+          initial={{ opacity: 0, x: 10 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
           className="invisible absolute -right-[10vw] -bottom-6 sm:visible sm:-right-[35vw] sm:scale-70 md:-right-[30vw] md:scale-80 lg:-right-[10vw] xl:scale-90 2xl:-right-[5vw]"
-        />
+        >
+          <Image src={Cloud} alt="Cloud" />
+        </motion.div>
       </div>
 
       <Registration />
-      <Image
-        src={JoinDragon}
-        alt="Dragon Riding a Wave"
-        className="-mt-30 hidden w-full object-fill sm:block"
-      />
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+        viewport={{ once: true, amount: 0.3 }}
+        className="-mt-15 hidden w-full overflow-hidden sm:block"
+      >
+        <Image
+          src={JoinDragon}
+          alt="Dragon Riding a Wave"
+          className="h-auto w-full object-cover"
+        />
+      </motion.div>
     </div>
   );
 };
